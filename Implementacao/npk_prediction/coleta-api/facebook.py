86846fd9e667f38ac21b895737d51026c7f90b40
nativanando <nativanando@gmail.com>
2017-07-12 17:09:40 -0300

    adicionando graficos iniciais das series temporais coletadas

--- Implementacao/npk_prediction/coleta-api/facebook.py
+++ Implementacao/npk_prediction/coleta-api/facebook.py
@@ -1,9 +1,5 @@
 import pandas as pd
-import plotly.offline as py
-import plotly.plotly as py
-import plotly.graph_objs as go
-import plotly
-plotly.tools.set_credentials_file(username='nativanando', api_key='jT97SpDbDMxAEXWmBhyM')
+import matplotlib.pyplot as plt
 
 
 dataset = pd.read_csv('~/Documentos/TCC/dist-tcc/Implementacao/dados/facebook.txt')
@@ -12,19 +8,27 @@ dataset['Date'] = pd.to_datetime(dataset['Date']) ##cast de data
 dataset['Variation'] = dataset['Close'].sub(dataset['Open']) ##variacao entre a abertura e o fechamento
 print(dataset.head()) ##informacoes gerais sobre o dataset
 
-x1= dataset.Date
+dataset1 = pd.read_csv('~/Documentos/TCC/dist-tcc/Implementacao/dados/microsoft.txt')
+dataset1.head() ##informacoes sobre a base (colunas etc)
+dataset1['Date'] = pd.to_datetime(dataset1['Date']) ##cast de data
+dataset1['Variation'] = dataset1['Close'].sub(dataset1['Open']) ##variacao entre a abertura e o fechamento
+print(dataset1.head()) ##informacoes gerais sobre o dataset
+
+x1=dataset.Date
 y1=dataset.Close
-data = [go.Scatter(x=x1, y=y1)]
-layout = go.Layout(
-   xaxis=dict(
-       range=['01-01-2010','11-04-2017'],
-       title='Ano'
-   ),
-   yaxis=dict(
-       range=[min(x1), max(y1)],
-       title='Valor da Acao'
-   ))
-fig = go.Figure(data = data, layout = layout)
-py.iplot(fig)
+
+z1=dataset1.Date
+z2=dataset1.Close
+
+fig, ax = plt.subplots()
+
+ax.plot(z1,z2)
+ax.plot(x1,y1)
+
+plt.show()
+plt.grid(True)
+plt.savefig('imagem.jpeg')
+plt.close()
+
 
 
